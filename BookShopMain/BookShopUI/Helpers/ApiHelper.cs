e44a71cf9505e6df9e0c4189322f9c26f56d1e43
Damian Stępczyński <damian030306@wp.pl>
2020-07-07 17:41:37 +0200

    Login UI

---
+++ BookShopMain/BookShopUI/Helpers/ApiHelper.cs
@@ -0,0 +1,59 @@
+﻿using BookShopUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BookShopUI.Helpers
+{
+    public class APIHelper : IAPIHelper
+    {
+        public HttpClient apiClient;
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            string webAdress = ConfigurationManager.AppSettings["webAdress"];
+
+            apiClient = new HttpClient();
+            apiClient.BaseAddress = new Uri(webAdress);
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<AuthenticatedUser> Authenticate(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password),
+                new KeyValuePair<string, string>("grant_type", "password")
+            });
+
+            using (HttpResponseMessage response = await apiClient.PostAsync("/Token", data))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        //public async Task<Author> GetAuthorAsync(string token)
+    }
+}
+
---
+++ BookShopMain/BookShopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private LoginViewModel loginVM1;
+
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            loginVM1 = loginVM;
+            ActivateItem(loginVM1);
+        }
+    }
+}
